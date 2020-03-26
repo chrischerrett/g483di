e4eddf01600838cf9314d5cb3354209899e1cddb
Chris Cherrett <chris.cherrett@gilmond.com>
2020-03-26 10:34:23 +0000

    endpoints

--- src/static/api.ts
+++ src/static/api.ts
@@ -2,7 +2,7 @@ import fetch from "cross-fetch";
 import queryString from "query-string";
 
 export async function api(method: "GET" | "POST" | "PATCH" | "PUT" | "DELETE", path: string, params?: {}, data?: any) {
-    return fetch(`http://gilmond-tfst3:44358/${path}${params ? "?" + queryString.stringify(params) : ""}`, {
+    return fetch(`https://myt3.gilmond.com:44305/SHP_Prototype/${path}${params ? "?" + queryString.stringify(params) : ""}`, {
         body: JSON.stringify(data),
         headers: {
             Accept: "application/json",
